27ae2cbb029cc2c1e0f88f152ec1e4448fb967e2
Muhammad Faizan <701765@student.inholland.nl>
2024-03-01 00:14:02 +0100

    Reset Pass page

--- app/controllers/ForgotPasswordController.php
+++ app/controllers/ForgotPasswordController.php
@@ -1,18 +1,27 @@
 <?php
+
+namespace App\Controllers;
+use App\services\UserService;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../vendor/autoload.php';
+
+
 class ForgotPasswordController
 {
-    private UserService $userService;
+    private UserService $forgotPasswordService;
 
-    public function __construct(UserService $userService)
+    public function __construct()
     {
-        $this->userService = $userService;
+        $this->forgotPasswordService = new userService();
     }
 
     public function resetPassword()
     {
         if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['email'])) {
             $email = $_POST['email'];
-            $user = $this->userService->getUserByEmail($email);
+            $user = $this->forgotPasswordService->getUserByEmail($email);
             if ($user) {
                 // Generate a unique token for password reset link
                 $token = bin2hex(random_bytes(32));
@@ -38,7 +47,7 @@ class ForgotPasswordController
 
     private function sendResetPasswordEmail($email, $reset_link): void
     {
-        $user = $this->userService->getUserByEmail($email);
+        $user = $this->forgotPasswordService    ->getUserByEmail($email);
         $name = $user['name'];
 
         // Instantiate PHPMailer
@@ -67,7 +76,9 @@ class ForgotPasswordController
             throw new Exception("Error: " . $mail->ErrorInfo);
         }
     }
-    public function setNewPassword(){
 
+    public function setNewPassword()
+    {
+        // Logic for setting new password
     }
 }
--- app/views/ResetPassword.php
+++ app/views/ResetPassword.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reset Password</title>
-    <?php include __DIR__ . '/../public/backend/css/style.css'; ?>
+    <style>    <?php include __DIR__ . '/../public/backend/css/style.css'; ?>
+    </style>
 </head>
 <body>
 <div class="container">
