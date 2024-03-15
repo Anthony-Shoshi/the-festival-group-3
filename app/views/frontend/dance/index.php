ec64dfe4d8ec612330b5a6352340352297f57617
Muhammad Faizan <701765@student.inholland.nl>
2024-03-15 11:15:24 +0100

    new

--- app/views/frontend/dance/index.php
+++ app/views/frontend/dance/index.php
@@ -1,10 +1,36 @@
 <?php include __DIR__ . '/../inc/header.php'; ?>
 
-<head><title>Dance</title></head>
+    <link rel="stylesheet" href="/frontend/css/dance.css"/>
+    <title>Dance</title>
+    </head>
 
-<div id="section-1">
-    <div class="festival-image">
-        <img src="/images/dance-head.jpg" alt=""/>
-        <h1>DANCE</h1>
+    <div id="section-dance">
+        <div class="dance-image">
+            <img src="/images/dance-head.jpg" alt=""/>
+            <h1>DANCE!</h1>
+        </div>
     </div>
-</div>
+
+    <div class="section-2">
+        <h2>Our Artists</h2>
+        <section>
+            <div class="artist-row-1">
+                <div class="artist-1">
+                    <img src="/images/Tiesto.jpg" alt=""/>
+                    <h3>Artist 1</h3>
+                </div>
+                <div class="artist-2">
+                <img src="/images/Armin van buren.jpeg" alt=""/>
+                <h3>Artist 2</h3>
+                </div>
+                 <div class="artist">
+                <div class="artist-3"
+                <img src="/images/Martin Garrix.jpg" alt=""/>
+                <h3>Artist 3</h3>
+                 </div>
+            </div>
+        </section>
+    </div>
+
+
+<?php include __DIR__ . '/../inc/footer.php'; ?>
