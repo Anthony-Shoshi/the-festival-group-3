13053826bdb7d06454d101aeacd0428a7cdc5335
Dawoodikram482 <mianmdawood482@gmail.com>
2024-03-21 22:19:22 +0100

    new changes added

--- app/controllers/HistoryController.php
+++ app/controllers/HistoryController.php
@@ -1,14 +1,17 @@
 <?php
 namespace App\Controllers;
 use App\Services\HistoryService;
+use App\Services\PageService;
 use Exception;
 
 class HistoryController
 {
     private HistoryService $historyService;
+    private PageService $pageService;
     public function __construct()
     {
         $this->historyService = new HistoryService();
+        $this->pageService = new PageService();
     }
     public function index()
     {
@@ -16,6 +19,8 @@ class HistoryController
             $location = $this->historyService->getAllTourLocations();
             $contents = $this->historyService->getAllContent();
             $tours = $this->historyService->getAllTours();
+            $pages = $this->pageService->getAllPages();
+
             require_once __DIR__ . '/../views/frontend/history/index.php';
         }catch (Exception $e) {
             header("Location: /error?message=" . urlencode($e->getMessage()));
